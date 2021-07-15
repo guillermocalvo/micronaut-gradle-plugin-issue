45a56b869f8d835a0ce5d9184ad93fb069422c21
Guillermo Calvo <guillermo@guillermo.dev>
2021-07-15 18:31:03 +0200

    Replace explicit constructor with Lombok annotation

--- src/main/java/foobar/FooController.java
+++ src/main/java/foobar/FooController.java
@@ -4,17 +4,15 @@ import io.micronaut.context.annotation.Context;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import javax.inject.Inject;
+import lombok.RequiredArgsConstructor;
 
 @Context
 @Controller
+@RequiredArgsConstructor
 public class FooController {
 
   @Inject
   private final FooService foo;
-
-  FooController(FooService foo){
-    this.foo = foo;
-  }
 
   @Get("/")
   String hello(){
